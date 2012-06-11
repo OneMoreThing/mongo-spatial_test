85d9fa99d4d28e43d33cc56149aea131f79f9498
yak <yakxxx@gmail.com>
2012-06-11 02:14:54 +0200

    work on cases

--- cases/cases.js
+++ cases/cases.js
@@ -1,3 +1,20 @@
+/**
+ * Test Case classes - Single test case means a test for a points in database with given extent
+ * and test_points. Test Case performs set of queries centered in "test_points" elements and of range in "extent".
+ * Summarized time of all queries and summarized  number of points returned by all queries are
+ * given as returning value of do_test(). {'millis': time, 'count': number} 
+ * 
+ * extent is range of test, ie. limit of searched points in $near query or radius of
+ * circle in $within query
+ * 
+ * test_points is list of points ie. [{'x':1,'y':1},{'x':2, 'y':7},...]. which are centers of "find" queries. 
+ * For example: center of circle, point in $near query,
+ */
+
+
+/**
+ * TestCase testing {$near: point} query
+ */
 var NearCase = function(extent, world_box, test_points, collection){
 	this.init(extent, world_box, test_points, collection);
 	
@@ -14,27 +31,36 @@ NearCase.prototype.do_test = function(){
 	
 	_this = this;
 	last_error = 0;
+	var exec_time = 0;
+	var point_count = 0;
 	
 	this.test_points.forEach(function(point){
-		near_points = _this.do_db_query(point);
+		ret = _this.do_db_query(point);
+		exec_time += ret.millis;
+		point_count += ret.count;
 		last_error = db.getLastError();
 		if(last_error != null){
-			return;
+			return null;
 		}
 	});
 	
-	return last_error == null;
+	return {'millis': exec_time, 'count': point_count};
 
 };
 
 
 NearCase.prototype.do_db_query = function(point){
-	db[this.collection].find({loc: {$near: [point.x, point.y]}}).limit(this.extent);
+	return count_time_and_results(this, function(){
+		var found_points = db[this.collection].find({loc: {$near: [point.x, point.y]}}).limit(this.extent);
+		return found_points;
+	});
 };
 
 
 
-
+/**
+ * TestCase testing {$within: {$box: ... }} query
+ */
 var WithinRectCase = function(extent, world_box, test_points, collection){
 	NearCase.prototype.init.call(this, extent, world_box, test_points, collection);
 };
@@ -44,11 +70,92 @@ WithinRectCase.constructor = WithinRectCase;
 
 WithinRectCase.prototype.do_db_query = function(point){
 	var box = [[point.x - this.extent, point.y - this.extent], [point.x + this.extent, point.y + this.extent]];
-	var found_points = db[this.collection].find({ loc: { $within: {$box: box} } });
-	var count = 0;
+	return count_time_and_results(this, function(){
+		var found_points = db[this.collection].find({ loc: { $within: {$box: box} } });
+		return found_points;
+	});
+};
+
+
+
+/**
+ * TestCase testing {$within: {$radius: ... }} query
+ */
+var WithinCircleCase = function(extent, world_box, test_points, collection){
+	NearCase.prototype.init.call(this, extent, world_box, test_points, collection);
+};
+
+WithinCircleCase.prototype = new NearCase();
+WithinCircleCase.constructor = WithinCircleCase; 
+
+WithinCircleCase.prototype.do_db_query = function(point){
+	var center = point;
+	var radius = this.extent;
+	var circle = [center, radius];
+	return count_time_and_results(this, function(){
+		var found_points = db[this.collection].find({ loc: { $within: {$center: circle } } });
+		return found_points;
+	});
 	
-	found_points.forEach(function(p){
+
+};
+
+
+
+/**
+ * TestCase testing {$within: {$polygon: ... }} query with polygon being hexagon
+ */
+var WithinHexagonCase = function(extent, world_box, test_points, collection){
+	NearCase.prototype.init.call(this, extent, world_box, test_points, collection);
+};
+
+WithinHexagonCase.prototype = new NearCase(); 
+WithinHexagonCase.constructor = WithinHexagonCase;
+
+WithinHexagonCase.prototype.do_db_query = function(point){
+	var hexagon = new RegularPolygon(6, point, this.extent);
+	return count_time_and_results(this, function(){
+		var found_points = db[this.collection].find({ loc: { $within: {$polygon: hexagon.vertices } } });
+		return found_points;
+	});
+	
+};
+
+
+
+/**
+ * Helper class creating regular polygon
+ */
+var RegularPolygon = function(num_of_vertices, center, radius){
+	this.vertices = [];
+	var angle = 2 * Math.PI / num_of_vertices;
+	for (var i = 0; i < num_of_vertices; i++){
+	    var x = center.x + radius * Math.sin(i * angle);
+	    var y = center.y + radius * Math.cos(i * angle);
+	    this.vertices.push({'x': x, 'y': y}); 
+	}
+
+};
+
+
+
+var count_time_and_results = function(ctx, callback){
+	// Warning! callback, count_results, profiling must be done in exactly this order.
+	// count_results is in fact triggering query in database which is lazy-loaded before that
+	// count_results makes access to all results to make sure query was fully fired.
+	var callback_ret = callback.call(ctx); 
+	var count = count_results(callback_ret)
+	var profile = db.system.profile.find().sort({$natural:-1}).limit(1).next();
+	return {'count': count, 'millis': profile.millis};
+};
+
+var count_results = function(cursor){
+	// must be explicit counting by iteration. 
+	// cursor.count() won't work because of some strange in-db optimizations
+	var count = 0;
+	cursor.forEach(function(p){
 		count++;
 	});
-	print(count);
+	return count;
 };
+
--- cases/cases_test.js
+++ cases/cases_test.js
@@ -1,7 +1,7 @@
 var case_test = function(){
 	
 	this.setUp = function(){
-		db.spat_autotests.drop();
+		//db.spat_autotests.drop();
 		this.world = new Field(5000, 5000, 10000, 10000);
 		/*
 		 * test points:
@@ -11,7 +11,7 @@ var case_test = function(){
 		 * |  x  |
 		 *  -----
 		 * */
-		this.test_points_bar = 500;
+		this.test_points_bar = 976;
 		this.test_points = [{
 								'x': this.world.center_x + this.world.width_x / 2 - this.test_points_bar,
 								'y': this.world.center_y 
@@ -33,17 +33,18 @@ var case_test = function(){
 								'y': this.world.center_y - this.world.width_y / 2 + this.test_points_bar
 							}
 		                   ];
+//		var uni_gen = new UniformDataGenerator(this.world, 'spat_autotests');
+//		uni_gen.generate_data(1000000);
+//		db['spat_autotests'].ensureIndex({ loc: "2d"}, {min: 0, max: 10000, bits: 26});
 	};
 	
-	this.do_test_test = function(case_class, label){
-		this.setUp();
-		var uni_gen = new UniformDataGenerator(this.world, 'spat_autotests');
-		uni_gen.generate_data(10000);
-		db['spat_autotests'].ensureIndex({ loc: "2d"}, {min: 0, max: 10000, bits: 26});
-		var near_case = new case_class(this.test_points_bar, this.world, this.test_points, 'spat_autotests');
-		ret = near_case.do_test();
-		assert(ret == true);
-		print('OK - near_test ' + label);
+	this.do_test_test = function(case_class, label, extent){
+		extent = extent || this.test_points_bar;
+		
+		var my_case = new case_class(extent, this.world, this.test_points, 'spat_autotests');
+		ret = my_case.do_test();
+		assert(ret != null);
+		print('OK - test ' + label + ' in: '+ret.millis+'ms. and ' + ret.count + ' objects found ');
 	};
 	
 	this.do_test_near = function(){
@@ -54,9 +55,39 @@ var case_test = function(){
 		this.do_test_test(WithinRectCase, 'within box');
 	};
 	
+	this.do_test_within_circle = function(){
+		this.do_test_test(WithinCircleCase, 'within circle');
+	};
 	
+	
+	this.regular_polygon_test_square = function(){
+		var eq = this.eq;
+		rp = new RegularPolygon(4, {'x': 1, 'y': 1}, 2);
+		var v_0 = eq(rp.vertices[0].x, 1) && eq(rp.vertices[0].y, 3);
+		var v_1 = eq(rp.vertices[1].x, 3) && eq(rp.vertices[1].y, 1);
+		var v_2 = eq(rp.vertices[2].x, 1) && eq(rp.vertices[2].y, -1);
+		var v_3 = eq(rp.vertices[3].x, -1) && eq(rp.vertices[3].y, 1);
+		assert(v_0 && v_1 && v_2 && v_3);
+		print('OK - regular_polygon square test');
+	};
+	
+	
+	this.eq = function(a,b){
+		return (Math.abs(a-b) < 0.0000001);
+	};
+	
+	this.do_test_within_hexagon = function(){
+		this.do_test_test(WithinHexagonCase, 'within Hexagon');
+	};
+	
+	
+	
+	this.setUp();
 	this.do_test_near();
 	this.do_test_within_box();
+	this.do_test_within_circle();
+	this.regular_polygon_test_square();
+	this.do_test_within_hexagon();
 	
 };
 
